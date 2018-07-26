214340e04a06b85731f9872fa8a06c8b9f4102fe
Bogdan Alexandru Cioropina <bogdan.cioropina@gmail.com>
2018-07-26 11:45:01 +0300

    - modified the starting file to reflect previous changes;

--- runner.php
+++ runner.php
@@ -5,42 +5,40 @@ define('XML_DATA', __DIR__ . DS . 'json-data' . DS . 'cards.xml');
 
 require_once __DIR__ . DS . 'vendor' . DS . 'autoload.php';
 
+$helper = new \TripSorter\Helper\TripSorterHelper();
+
 // running with JSON data provider
 try {
-    if (\TripSorter\Misc\Util::isFileValid(JSON_DATA)) {
+    if ($helper->isFileValid(JSON_DATA)) {
         $json = file_get_contents(JSON_DATA);
         $reader = \TripSorter\DataReader\DataReaderFactory::createReader(
             \TripSorter\DataReader\DataReaderType::JSON
         );
         $rawData = $reader->convertFromString($json);
-        $boardingCards = \TripSorter\Misc\Util::createCardsList($rawData);
+        $boardingCards = $helper->createCardsList($rawData);
 
         $sorter = new \TripSorter\TripSorter($boardingCards);
         $tripList = $sorter->retrieveSortedTrip();
     }
-} catch (InvalidArgumentException $iae) {
-    echo 'An error occurred: ' . $iae->getMessage() . PHP_EOL;
 } catch (Exception $exception) {
     echo 'A generic error occurred: ' . $exception->getMessage() . PHP_EOL;
 }
 
 // now, doing the same thing, only with xml as a data provider.
 try {
-    if (\TripSorter\Misc\Util::isFileValid(JSON_DATA)) {
+    if ($helper->isFileValid(JSON_DATA)) {
         $xml = file_get_contents(XML_DATA);
         $reader = \TripSorter\DataReader\DataReaderFactory::createReader(
             \TripSorter\DataReader\DataReaderType::XML
         );
         $rawData = $reader->convertFromString($xml);
-        $boardingCards = \TripSorter\Misc\Util::createCardsList($rawData);
+        $boardingCards = $helper->createCardsList($rawData);
     }
-} catch (InvalidArgumentException $iae) {
-    echo 'An error occurred: ' . $iae->getMessage() . PHP_EOL;
 } catch (Exception $exception) {
     echo 'A generic error occurred: ' . $exception->getMessage() . PHP_EOL;
 }
 
 foreach($tripList as $card) {
-    echo \TripSorter\Misc\Util::stringifyCard($card) . PHP_EOL;
+    echo $helper->stringifyCard($card) . PHP_EOL;
 }
-echo 'You have arrived at your final destination.' . PHP_EOL;
+echo \TripSorter\Constant\StringConstant::TRIP_FINAL_MESSAGE;
