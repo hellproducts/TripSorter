e0f8f5cb469c4625a2a494b84fe202ca89367587
Bogdan Cioropina <bogdan.cioropina@gmail.com>
2018-07-26 07:53:03 +0300

    - find first and last trip node; - implement bubble sort algorithm to create final cards stack;

--- src/TripSorter.php
+++ src/TripSorter.php
@@ -1,5 +1,8 @@
 <?php
+
 namespace TripSorter;
+
+use TripSorter\BoardingCard\Card;
 
 /**
  * Class TripSorter
@@ -11,16 +14,94 @@ class TripSorter
 
     /** @var array */
     private $cards;
+    /** @var int */
+    private $cardsNo;
+
+    public function __construct(array $cards)
+    {
+        $this->cards = $cards;
+        $this->cardsNo = count($cards);
+    }
 
     /**
-     * This method takes a json string as an argument, attempts to deserialize it and to build the
-     * needed Card objects.
-     *
-     * @param string $jsonArray
+     * @return array
      */
-    public function loadBoardingCards(string $jsonArray): void
+    public function retrieveSortedTrip(): array
     {
-
-
+        $this->identifyDepartureAndDestination();
+        $this->resetListIndexes();
+        $this->sortCards();
+        return $this->cards;
     }
+
+
+    private function identifyDepartureAndDestination(): void
+    {
+        for ($i = 0; $i < $this->cardsNo; $i++) {
+            $hasPreviousPoint = false;
+            $isEndingPoint = true;
+            // loop through all the cards and identify 1st and last node in the trip
+            foreach ($this->cards as $index => $card) {
+                $this->compareCards(
+                    $this->cards[$i],
+                    $card,
+                    $hasPreviousPoint,
+                    $isEndingPoint
+                );
+            }
+
+            if (!$hasPreviousPoint) {
+                array_unshift($this->cards, $this->cards[$i]);
+                unset($this->cards[$i]);
+            }
+            if ($isEndingPoint) {
+                $this->cards[] = $this->cards[$i];
+                unset($this->cards[$i]);
+            }
+        }
+    }
+
+    private function sortCards(): void
+    {
+        for ($i = 0; $i < $this->cardsNo; $i++) {
+            /** @var Card $currentCard */
+            /** @var Card $card */
+            $currentCard = $this->cards[$i];
+            foreach ($this->cards as $idx => $card) {
+
+                if (strcasecmp($currentCard->getTo(), $card->getFrom()) === 0) {
+                    $nextIdx = $i + 1;
+                    $tmp = $this->cards[$nextIdx];
+                    $this->cards[$nextIdx] = $card;
+                    $this->cards[$idx] = $tmp;
+                    break;
+                }
+            }
+        }
+    }
+
+    /**
+     *
+     */
+    private function resetListIndexes(): void
+    {
+        $this->cards = array_values($this->cards);
+    }
+
+    /**
+     * @param Card $firstCard
+     * @param Card $secondCard
+     * @param      $hasPreviousTrip
+     * @param      $isLastTrip
+     */
+    private function compareCards(Card $firstCard, Card $secondCard, bool &$hasPreviousTrip, bool &$isLastTrip): void
+    {
+        if (strcasecmp($firstCard->getFrom(), $secondCard->getTo()) === 0) {
+            $hasPreviousTrip = true;
+        }
+        if (strcasecmp($firstCard->getTo(), $secondCard->getFrom()) === 0) {
+            $isLastTrip = false;
+        }
+    }
+
 }
