2d0a05ed3dcde2e5fb5803442fe4b095c9bec417
Bogdan Cioropina <bogdan.cioropina@gmail.com>
2018-07-26 07:52:01 +0300

    - changed one transport type;

--- src/BoardingCard/CardType.php
+++ src/BoardingCard/CardType.php
@@ -5,6 +5,6 @@ namespace TripSorter\BoardingCard;
 class CardType
 {
     public const TYPE_PLANE = 'plane';
-    public const TYPE_BUS = 'bus';
+    public const TYPE_BUS = 'airport bus';
     public const TYPE_TRAIN = 'train';
 }
